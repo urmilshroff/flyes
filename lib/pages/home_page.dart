6f899e16d8f4b0c16a09a3b1ee9880137f41b0ce
urmilshroff <urmilshroff@gmail.com>
2020-04-07 17:30:09 +0530

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flyes/utils/colors.dart';
 import 'package:neumorphic/neumorphic.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MyColors.accent,
       appBar: NeuAppBar(
         title: Text('Flyes'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          SizedBox(
+            height: 50.0,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              NeuCard(
-                  curveType: CurveType.concave,
+              Container(
+                height: 150.0,
+                width: 150.0,
+                child: NeuCard(
+                  curveType: CurveType.flat,
                   bevel: 12,
                   decoration: NeumorphicDecoration(
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Text('Container')),
-              NeuButton(
-                onPressed: () {
-                  print('Button pressed');
-                },
-                child: Text('Button'),
+                    color: MyColors.accent,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 15.0,
+              ),
+              Container(
+                height: 150.0,
+                width: 150.0,
+                child: NeuButton(
+                  shape: BoxShape.circle,
+                  onPressed: () {
+                    print('Button pressed');
+                  },
+                ),
               ),
             ],
           ),
